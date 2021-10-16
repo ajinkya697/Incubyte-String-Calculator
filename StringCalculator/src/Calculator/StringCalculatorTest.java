e9beb213e17f7c0f4dbbe387ede577822e974626
Ajinkya Desai <ajinkyadesai697@gmail.com>
2021-10-16 19:03:37 +0530

    Support different delimiters

--- StringCalculator/src/Calculator/StringCalculatorTest.java
+++ StringCalculator/src/Calculator/StringCalculatorTest.java
@@ -29,4 +29,8 @@ public class StringCalculatorTest {
 	    Assert.assertEquals(3+6+15, StringCalculator.add("3,6n15"));
 	}
 	
+	@Test
+	public final void whenDelimiterIsSpecifiedThenItIsUsedToSeparateNumbers() {
+	    Assert.assertEquals(3+6+15, StringCalculator.add("//;n3;6;15"));
+	}
 }
