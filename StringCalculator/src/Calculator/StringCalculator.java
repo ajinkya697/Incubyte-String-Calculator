6809e310e0fe888fdcb133339f414cf62fcde19e
Ajinkya Desai <ajinkyadesai697@gmail.com>
2021-10-16 18:12:17 +0530

    second commit

--- StringCalculator/src/Calculator/StringCalculator.java
+++ StringCalculator/src/Calculator/StringCalculator.java
@@ -2,16 +2,16 @@ package Calculator;
 
 public class StringCalculator {
 	public static int add(String numbers) {
-		String[] numbersArray = numbers.split(",");
+		int returnValue = 0;
+	    String[] numbersArray = numbers.split(",");
 	    if (numbersArray.length > 2) {
 	        throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
-	    } else {
-	        for (String number : numbersArray) {
-	            if (!number.isEmpty()) {
-	                Integer.parseInt(number);
-	            }
+	    }
+	    for (String number : numbersArray) {
+	        if (!number.trim().isEmpty()) { // After refactoring
+	            returnValue += Integer.parseInt(number);
 	        }
 	    }
-	    return 0; // Added return
+	    return returnValue;
     }
 }
